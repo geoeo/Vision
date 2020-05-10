052a8f7ea1000f05c1f4a78c81fcf241677e3b1a
Marc Haubenstock <marc.haubenstock@outlook.com>
2020-05-10 18:22:56 +0200

    octave code

--- src/pyramid/mod.rs
+++ src/pyramid/mod.rs
@@ -1,3 +1,6 @@
+extern crate image as image_rs;
+
+use image_rs::GrayImage;
 use self::octave::Octave;
 
 pub mod octave;
@@ -6,4 +9,14 @@ pub mod octave;
 pub struct Pyramid {
     pub octaves: Vec<Octave>,
     pub s: f32
+}
+
+impl Pyramid {
+    //pub fn build_pyramid(base_image: &GrayImage, s: f32, octave_count: u8) -> Pyramid {
+    pub fn build_pyramid(base_image: &GrayImage, s: f32, octave_count: u8) -> () {
+
+        //TODO
+
+
+    }
 }
--- src/pyramid/octave.rs
+++ src/pyramid/octave.rs
@@ -1,5 +1,6 @@
 extern crate image as image_rs;
 
+use std::ops::Range;
 use image_rs::{GrayImage, imageops::blur};
 use crate::image::Image;
 
@@ -11,20 +12,37 @@ pub struct Octave {
 
 impl Octave {
 
-    pub fn add(octave: &mut Octave, image: Image, sigma: f32) -> () {
+    pub fn build_octave(base_image: &GrayImage, s: u32, sigma_initial: f32) -> Octave {
+
+        let image_count = s + 3;
+        let range = 0..image_count;
+        let sigmas: Vec<f32> = range.map(|x| sigma_initial*Octave::generate_k(x as f32, s as f32)).collect();
+
+        let images = sigmas.iter().map(|&sigma| Octave::blur(base_image, sigma)).collect();
+
+        Octave {images,sigmas}
+    }
+
+    fn add(octave: &mut Octave, image: Image, sigma: f32) -> () {
         octave.images.push(image);
         octave.sigmas.push(sigma);
     }
 
-    pub fn blur(gray_image: &GrayImage, sigma: f32) -> Image {
+    fn blur(gray_image: &GrayImage, sigma: f32) -> Image {
         let blurred_gray_image = blur(gray_image,sigma);
         Image::from_gray_image(&blurred_gray_image)
     }
 
-    pub fn blur_and_add(octave: &mut Octave, gray_image: &GrayImage, sigma: f32) -> () {
+    fn blur_and_add(octave: &mut Octave, gray_image: &GrayImage, sigma: f32) -> () {
         let blurred_image = Octave::blur(gray_image, sigma);
 
         Octave::add(octave, blurred_image, sigma);
+    }
+
+    fn generate_k(n: f32, s: f32) -> f32 {
+        assert!(n <= s && n >= 0f32);
+        let exp = n/s;
+        exp.exp2()
     }
 }
 
