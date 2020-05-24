1d23b7b62100b3149f0bdfa26ef96f97bcfc44c6
Marc Haubenstock <marc.haubenstock@outlook.com>
2020-05-24 14:38:19 +0200

    extrema detection code

--- src/keypoint/mod.rs
+++ src/keypoint/mod.rs
@@ -17,14 +17,43 @@ pub fn detect_extrema(image: &Image, prev_image: &Image, next_image: &Image, x_s
 
             let sample_value = image_buffer[(y,x)];
 
-            //TODO: compare sample to 8 neighbours in image and 9 in prev and next
-            
+            //TODO: @Investigate parallel
+            let is_extrema = 
+                is_sample_extrema_in_neighbourhood(sample_value,x,y,image_buffer,true) && 
+                is_sample_extrema_in_neighbourhood(sample_value,x,y,prev_buffer,false) && 
+                is_sample_extrema_in_neighbourhood(sample_value,x,y,next_buffer,false);
+
+            if is_extrema {
+                extrema_vec.push((x,y));
+            }
         }
     }
 
     extrema_vec
 }
 
-fn is_sample_extrema_in_neighbourhood(sample: Float, x: usize, y: usize, neighbourhood_buffer: &DMatrix<Float>, skip_center: bool) -> bool {
-    false
+fn is_sample_extrema_in_neighbourhood(sample: Float, x_sample: usize, y_sample: usize, neighbourhood_buffer: &DMatrix<Float>, skip_center: bool) -> bool {
+
+    let mut is_smallest = true;
+    let mut is_largest = true;
+
+    for x in x_sample-1..x_sample+2 {
+        for y in y_sample-1..y_sample+2 {
+
+            if x == x_sample && y == y_sample && skip_center {
+                continue;
+            }
+
+            let value = neighbourhood_buffer[(y,x)];
+            is_smallest &= sample < value;
+            is_largest &= sample > value;
+
+            if !(is_smallest || is_largest) {
+                break;
+            }
+
+        }
+    }
+
+    is_smallest || is_largest
 }
