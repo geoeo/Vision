556266d165563b740fe6029d5a70c4fefde36000
Marc Haubenstock <marc.haubenstock@outlook.com>
2020-07-21 16:41:27 +0200

    orientation assigment functionally complete

--- src/descriptor/mod.rs
+++ src/descriptor/mod.rs
@@ -5,14 +5,24 @@ use crate::Float;
 
 pub mod orientation_histogram;
 
-pub fn lagrange_interpolation_quadratic(a: Float, b: Float, c: Float, f_a: Float, f_b: Float, f_c: Float) -> Float {
+pub fn lagrange_interpolation_quadratic(a: Float, b: Float, c: Float, f_a: Float, f_b: Float, f_c: Float, range_min: Float, range_max: Float) -> Float {
+
+    let a_corrected = if a > b { a - range_max} else {a};
+    let c_corrected = if b > c { c + range_max} else {c};
+
     assert!( a < b && b < c);
     assert!( f_a > f_b && f_b < f_c);
 
-    let numerator = (f_a-f_b)*(c-b).powi(2)-(f_c-f_b)*(b-a).powi(2);
-    let denominator = (f_a-f_b)*(c-b)+(f_c-f_b)*(b-a);
+    let numerator = (f_a-f_b)*(c_corrected-b).powi(2)-(f_c-f_b)*(b-a_corrected).powi(2);
+    let denominator = (f_a-f_b)*(c_corrected-b)+(f_c-f_b)*(b-a_corrected);
 
-    b + 0.5*(numerator/denominator)
+    let result  = b + 0.5*(numerator/denominator);
+
+    match result {
+        res if res < range_min => res + range_max,
+        res if res > range_max => res - range_max,
+        res => res
+    }
 }
 
 
--- src/descriptor/orientation_histogram.rs
+++ src/descriptor/orientation_histogram.rs
@@ -5,32 +5,36 @@ use na::{Matrix1x2,Matrix2};
 use crate::{float,Float, ExtremaParameters, KeyPoint};
 use crate::image::Image;
 use crate::pyramid::octave::Octave;
+use crate::descriptor::lagrange_interpolation_quadratic;
 
 
 #[derive(Debug,Clone)]
 pub struct OrientationHistogram {
     max_bin: usize,
+    pub bin_range: Float,
     pub bins: Vec<Float>
 }
 
 impl OrientationHistogram {
 
-    pub fn new(bin_size: usize) -> OrientationHistogram {
+    pub fn new(bin_len: usize) -> OrientationHistogram {
         OrientationHistogram{
-            max_bin: bin_size+1,
-            bins: vec![0.0;bin_size]
+            max_bin: bin_len,
+            bin_range: 2.0*float::consts::PI/(bin_len as Float),
+            bins: vec![0.0;bin_len]
         }
     }
 
-    //TODO check if this is missing a step -> Tthis is just for orientation!
     pub fn add_measurement(& mut self, grad_orientation: (Float,Float), weight: Float) -> () {
         let grad = grad_orientation.0;
         let orientation = grad_orientation.1;
-        let bin_range = 2.0*float::consts::PI/(self.bins.len() as Float);
-        let index = (orientation/bin_range).floor() as usize;
-
+        let index = (orientation/self.bin_range).floor() as usize;
         self.bins[index] += grad*weight;
+    }
 
+    pub fn index_to_radian(&self, index: Float) -> Float {
+        assert!(index >=0.0 && index < self.bins.len() as Float);
+        index*self.bin_range
     }
 
 }
@@ -98,11 +102,33 @@ pub fn process_window(octave: &Octave, keypoint: &ExtremaParameters) -> Vec<KeyP
         max
     }).0;
 
-    post_process(&histogram)
+    post_process(&histogram,keypoint)
 }
 
-fn post_process(histogram: &OrientationHistogram) ->  Vec<KeyPoint> {
-    //TODO: peak post processing
-    Vec::with_capacity(2)
+fn post_process(histogram: &OrientationHistogram, extrema: &ExtremaParameters) -> Vec<KeyPoint> {
+
+
+    let max_val = histogram.bins[histogram.max_bin];
+    let threshold = max_val*0.8;
+    let peaks_indices = histogram.bins.clone().into_iter().enumerate().filter(|x| x.1 >= threshold).map(|t| t.0).collect::<Vec<usize>>();
+    let peak_neighbours_indices = peaks_indices.iter().map(|&x| get_cirular_closest(histogram, x)).collect::<Vec<(usize,usize,usize)>>();
+    let interpolated_peaks_indices = peak_neighbours_indices.iter().map(|&(l,c,r)| 
+        lagrange_interpolation_quadratic(l as Float,c as Float,r as Float,histogram.bins[l],histogram.bins[c],histogram.bins[r],0.0, (histogram.bins.len() - 1) as Float)
+    ).collect::<Vec<Float>>();
+
+    interpolated_peaks_indices.iter().map(|&peak_idx| {KeyPoint{x: extrema.x, y: extrema.y, sigma_level: extrema.sigma_level, orientation: histogram.index_to_radian(peak_idx)}}).collect::<Vec<KeyPoint>>()
 
 }
+
+fn get_cirular_closest(histogram: &OrientationHistogram, bin_idx: usize) -> (usize,usize,usize) {
+    let bin_len = histogram.bins.len();
+    assert!(bin_len >=3);
+
+    match bin_idx {
+        idx if idx == 0 => (bin_len-1,idx,1),
+        idx if idx == bin_len - 1 => (idx-1,idx,0),
+        idx => (idx - 1,idx, idx + 1)
+    }
+    
+}
+
