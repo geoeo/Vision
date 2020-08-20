5a9127342593fa9dedc06ea5a0700798c9872afd
Marc Haubenstock <marc.haubenstock@outlook.com>
2020-08-20 22:06:25 +0200

    removed theshold

--- src/descriptor/feature_vector.rs
+++ src/descriptor/feature_vector.rs
@@ -29,7 +29,7 @@ impl FeatureVector {
             FeatureVector{data}
     }
 
-    pub fn distance_between(&self, other_feature_vector: &FeatureVector, threshold: Float) -> Float {
+    pub fn distance_between(&self, other_feature_vector: &FeatureVector) -> Float {
         let mut squared_distance = 0;
         for i in 0..DESCRIPTOR_BINS {
             for j in 0..ORIENTATION_BINS {
@@ -39,6 +39,6 @@ impl FeatureVector {
             }
         }
 
-        threshold*(squared_distance as Float).sqrt()
+        (squared_distance as Float).sqrt()
     }
 }
