45639eff7b58862b55feb9f7e2d98deb10d5226d
Marc Haubenstock <marc.haubenstock@outlook.com>
2020-11-20 23:59:13 +0100

    loading intrinsics

--- examples/load_tum.rs
+++ examples/load_tum.rs
@@ -5,19 +5,26 @@ use std::path::Path;
 use vision::io::tum_loader;
 
 fn main() {
-    let image_name = "img0001_0";
-    let image_format = "depth";
-    let image_folder = "C:/Users/Marc/Workspace/Datasets/ETH/urban_pinhole/rpg_urban_pinhole_depthmaps/data/depth/";
+    let depth_image_name = "img0001_0";
+    let intrinsics_name = "intrinsics";
+    let depth_image_format = "depth";
+    let intrinsics_format = "txt";
+    let depth_image_folder = "C:/Users/Marc/Workspace/Datasets/ETH/urban_pinhole/rpg_urban_pinhole_depthmaps/data/depth/";
+    let intrinsics_folder = "C:/Users/Marc/Workspace/Datasets/ETH/urban_pinhole/rpg_urban_pinhole_info/info/";
     let image_out_folder = "output/";
-    let depth_image_path = format!("{}{}.{}",image_folder,image_name, image_format);
-    let converted_file_out_path = format!("{}{}_out.png",image_out_folder,image_name);
+
+    let depth_image_path = format!("{}{}.{}",depth_image_folder,depth_image_name, depth_image_format);
+    let depth_converted_file_out_path = format!("{}{}_out.png",image_out_folder,depth_image_name);
+
+    let intrinsics_path = format!("{}{}.{}",intrinsics_folder,intrinsics_name, intrinsics_format);
+    println!("{}",intrinsics_path);
+
+    let depth_display = tum_loader::load_depth_image(&Path::new(&depth_image_path));
+    let intrinsics = tum_loader::load_intrinsics(&Path::new(&intrinsics_path));
 
 
-    let display = tum_loader::load_depth_image(&Path::new(&depth_image_path));
-
-    let new_image = display.to_image();
-
-    new_image.save(converted_file_out_path).unwrap();
+    let new_depth_image = depth_display.to_image();
+    new_depth_image.save(depth_converted_file_out_path).unwrap();
 
 
 }
--- src/io/tum_loader.rs
+++ src/io/tum_loader.rs
@@ -9,6 +9,7 @@ use std::io::{BufReader,Read};
 use crate::Float;
 use crate::io::parse_to_float;
 use crate::image::{Image,image_encoding::ImageEncoding};
+use crate::camera::pinhole::Pinhole;
 
 pub fn load_depth_image(file_path: &Path) -> Image {
     let file = File::open(file_path).expect("load_depth_map failed");
@@ -19,7 +20,6 @@ pub fn load_depth_image(file_path: &Path) -> Image {
     let rows = 480;
     let cols = 640;
     let mut matrix = DMatrix::<Float>::zeros(rows,cols);
-
 
     let values = contents.trim().split(" ").map(|x| parse_to_float(x)).collect::<Vec<Float>>();
     assert_eq!(values.len(),rows*cols);
@@ -33,6 +33,23 @@ pub fn load_depth_image(file_path: &Path) -> Image {
 }
 
 pub fn load_image(file_path: &Path) -> Image {
-    let gray_image = image_rs::open(&Path::new(&file_path)).unwrap().to_luma();
+    let gray_image = image_rs::open(&Path::new(&file_path)).expect("load_image failed").to_luma();
     Image::from_gray_image(&gray_image, false)
+}
+
+pub fn load_intrinsics(file_path: &Path) -> Pinhole {
+    let file = File::open(file_path).expect("load_intrinsics failed");
+    let mut reader = BufReader::new(file);
+    let mut contents = String::new();
+    reader.read_to_string(&mut contents).unwrap();
+    let values = contents.trim().split(|c| !char::is_numeric(c)).map(|s| (s.len(),s.parse::<f64>())).filter(|(_,option)|  option.is_ok()).map(|(len,option)| (len,option.unwrap())).collect::<Vec<(usize,Float)>>();
+
+    let fx = values[0].1 + values[1].1/(10f64.powi(values[1].0 as i32));
+    let fy = values[6].1 +values[7].1/(10f64.powi(values[7].0 as i32));
+
+    let cx = values[3].1;
+    let cy = values[8].1;
+
+    Pinhole::new(fx, fy, cx, cy)
+
 }
