58c595b85328f7a153ed072d6dff0bd66a53d451
Marc Haubenstock <marc.haubenstock@outlook.com>
2020-11-20 19:46:24 +0100

    fixed path

--- examples/load_tum.rs
+++ examples/load_tum.rs
@@ -5,11 +5,11 @@ use std::path::Path;
 use vision::io::tum_loader;
 
 fn main() {
-
-    let image_name = "img0008_0";
+    
+    //TODO: correct file path
+    let image_name = "img0001_0";
     let image_format = "depth";
-    //let image_folder = "C:/Users/Marc/Workspace/Datasets/ETH/urban_pinhole/rpg_urban_pinhole_depthmaps/data/depth/";
-    let image_folder = "images/";
+    let image_folder = "C:/Users/Marc/Workspace/Datasets/ETH/urban_pinhole/rpg_urban_pinhole_depthmaps/data/depth/";
     let image_out_folder = "output/";
     let depth_image_path = format!("{}{}.{}",image_folder,image_name, image_format);
     let converted_file_out_path = format!("{}{}_out.png",image_out_folder,image_name);
