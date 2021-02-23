6f5bf6640eca1a4e8ccc3f673851dc666571fd65
Marc Haubenstock <marc.haubenstock@outlook.com>
2021-02-23 22:59:00 +0100

    starting imu preintegration defintions

--- src/odometry/imu_odometry/mod.rs
+++ src/odometry/imu_odometry/mod.rs
@@ -1,2 +1,24 @@
+use nalgebra as na;
+
+use na::Vector3;
+use crate::Float;
+use crate::sensors::DataFrame;
+use crate::odometry::imu_odometry::{imu_state::ImuState,imu_noise_state::ImuNoiseState};
 
 
+pub mod imu_noise_state;
+pub mod imu_state;
+
+
+
+pub fn PreIntegration(data_frame: &DataFrame) -> (ImuState, ImuNoiseState) {
+
+
+    panic!("PreIntegration not yet implemented")
+
+}
+
+pub fn GravityEstimation(data_frame: &DataFrame) -> Vector3::<Float> {
+
+    panic!("Gravity Estimation Not yet implemented")
+}
