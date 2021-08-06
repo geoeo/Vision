0f329f5234d08d56790bddeba5a5ae2b740f22a5
Marc Haubenstock <marc.haubenstock@outlook.com>
2021-08-07 01:23:45 +0200

    estimated features

--- src/bundle_adjustment/solver.rs
+++ src/bundle_adjustment/solver.rs
@@ -1,44 +1,31 @@
 extern crate nalgebra as na;
 
 use na::{DVector,Matrix, Dynamic, U4, VecStorage};
-use crate::features::geometry::point::Point;
 use crate::Float;
 use crate::sensors::camera::Camera;
 use crate::numerics::lie::exp;
 
-//TODO: explicit links between camera params and featurs
-pub fn get_estimated_features<C : Camera>(estimated_state: &DVector<Float>, features_per_cam: &Vec<usize>, cameras: &Vec<&C>, n_cams: usize, n_features: usize) -> DVector<Float> {
-    panic!("not implemented");
-    let mut estimated_features = DVector::<Float>::zeros(n_features*2);
-    let features_per_cam_acc = features_per_cam.iter().scan(0, |acc, x| {
-        *acc = *acc + x;
-        Some(*acc)
-    }).collect::<Vec<usize>>();
+// In the format [f1_cam1,f2_cam1,f3_cam1,f1_cam2,f2_cam2,...]
+pub fn get_estimated_features<C : Camera>(estimated_state: &DVector<Float>, cameras: &Vec<&C>, n_cams: usize, n_points: usize, estimated_features: &mut DVector<Float>) -> () {
+    assert_eq!(estimated_features.nrows(),n_cams*n_points*2);
     for i in (0..n_cams).step_by(6){
         let u = estimated_state.fixed_rows::<3>(i*n_cams);
         let w = estimated_state.fixed_rows::<3>(i*n_cams+3);
-        let n_features_for_cam = features_per_cam[i];
         let pose = exp(&u,&w);
         let camera = cameras[i];
-        let offset = match i {
-            0 => n_cams,
-            _ => features_per_cam_acc[i-1]
-        };
 
-        let position_per_cam = Matrix::<Float,U4,Dynamic, VecStorage<Float,U4,Dynamic>>::from_element(n_features_for_cam, 1.0);
-        for j in (offset..features_per_cam_acc[i]).step_by(3) {
-            position_per_cam.fixed_rows_mut::<3>(j-offset).copy_from(&estimated_state.fixed_rows::<3>(j));
+        let mut position_per_cam = Matrix::<Float,U4,Dynamic, VecStorage<Float,U4,Dynamic>>::from_element(n_points, 1.0);
+        for j in (0..n_points).step_by(3) {
+            position_per_cam.fixed_rows_mut::<3>(j).copy_from(&estimated_state.fixed_rows::<3>(n_cams+j));
 
         };
         let transformed_points = pose*position_per_cam;
-        let mut points = Vec::<Point<Float>>::with_capacity(n_features_for_cam);
-        for j in 0..n_features_for_cam {
-            points.push(camera.project(&transformed_points.fixed_slice::<3,1>(0,j)));
+        for j in 0..n_points {
+            let estimated_feature = camera.project(&transformed_points.fixed_slice::<3,1>(0,j));            
+            estimated_features[i*n_points+2*j] = estimated_feature.x;
+            estimated_features[i*n_points+2*j+1] = estimated_feature.y;
         }
 
-        //TODO:put this in estimated features
     }
-
-    estimated_features
 
 }
