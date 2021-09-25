00e7958922338964d6a219ca2ccdd665046506e8
Marc Haubenstock <marc.haubenstock@outlook.com>
2021-09-25 14:24:34 +0200

    fixed measurement vectors

--- src/image/bundle_adjustment/solver.rs
+++ src/image/bundle_adjustment/solver.rs
@@ -10,8 +10,8 @@ use crate::odometry::runtime_parameters::RuntimeParameters; //TODO remove depend
 
 /**
  * In the format [f1_cam1_x,f1_cam1_y,f2_cam1_x,f2_cam1_y,f3_cam1_x,f3_cam1_y,f1_cam2_x,f1_cam2_y,f2_cam2_x,...]
+ * Some entries may be 0 since not all cams see all points
  * */
-//TODO: this is wrong -> observed features has to be reworked then this
 pub fn get_estimated_features<C : Camera>(state: &State, cameras: &Vec<C>, estimated_features: &mut DVector<Float>) -> () {
     let n_cams = state.n_cams;
     let n_points = state.n_points;
@@ -22,6 +22,7 @@ pub fn get_estimated_features<C : Camera>(state: &State, cameras: &Vec<C>, estim
         let w = estimated_state.fixed_rows::<3>(i*n_cams+3);
         let pose = exp(&u,&w);
         let camera = &cameras[i];
+        let offset = 2*i*n_points;
 
         let mut position_per_cam = Matrix::<Float,U4,Dynamic, VecStorage<Float,U4,Dynamic>>::from_element(3*n_points, 1.0);
         for j in (0..position_per_cam.nrows()).step_by(3) {
@@ -30,8 +31,8 @@ pub fn get_estimated_features<C : Camera>(state: &State, cameras: &Vec<C>, estim
         let transformed_points = pose*position_per_cam;
         for j in 0..n_points {
             let estimated_feature = camera.project(&transformed_points.fixed_slice::<3,1>(0,j));            
-            estimated_features[i*n_points+2*j] = estimated_feature.x;
-            estimated_features[i*n_points+2*j+1] = estimated_feature.y;
+            estimated_features[offset+2*j] = estimated_feature.x;
+            estimated_features[offset+2*j+1] = estimated_feature.y;
         }
 
     }
