dfc85a3e85d4d294df8d6bc25cda44548a19a596
Marc Haubenstock <marc.haubenstock@outlook.com>
2022-01-29 21:12:34 +0100

    calculating sigmas

--- src/numerics/linear_prog.rs
+++ src/numerics/linear_prog.rs
@@ -4,11 +4,14 @@ use na::{OMatrix, OVector, DVector, DMatrix, Const, Vector2};
 use crate::Float;
 
 /**
- * Using Primal-Dual Algorithm. We recast L1-Norm as the sum of the inequalities i.e. Min(L1) => Min(c (= f0)) subject to Ax-y-c <= 0 and + -Ax+y-c <=0 
+ * Using Primal-Dual Algorithm. We recast L1-Norm as the sum of the inequalities i.e. Min(L1) => Min x,c (c (= f0)) subject to Ax-y-c <= 0 and + -Ax+y-c <=0 
  */
 
 #[allow(non_snake_case)]
-pub fn l1_norm_approx<const N: usize, const M: usize>(measurements: &OVector<Float,Const<M>>, A: &OMatrix<Float, Const<M>,Const<N>>,  state: &OVector<Float,Const<N>>) -> () {
+pub fn l1_norm_approx<const N: usize, const M: usize>(measurements: &OVector<Float,Const<M>>, A: &OMatrix<Float, Const<M>,Const<N>>,  state: &mut OVector<Float,Const<N>>) -> () {
+
+    let max_iter = 200;
+    let tol = 1e-3;
 
     let alpha = 0.01;
     let beta = 0.5;
@@ -22,20 +25,29 @@ pub fn l1_norm_approx<const N: usize, const M: usize>(measurements: &OVector<Flo
     f_0_grad.fixed_rows_mut::<N>(0).copy_from(&f_0_grad_x);
     f_0_grad.fixed_rows_mut::<M>(N).copy_from(&f_0_grad_u);
     let A_t = A.transpose();
+    let ones = DVector::<Float>::repeat(N*2,1.0);
 
 
 
-    let c = compute_f_0(measurements,A,state);
-    let h_init = compute_h(measurements,A,state,&c);
-    let mut u_vec = compute_u(&h_init,1.0); //lamu in l1magic 
+    let mut c = f_0(measurements,A,state);
+    let mut h = h(measurements,A,state,&c);
+    let mut h_recip = h.map(|v| v.recip());
+    let mut u_vec = u(&h,1.0); //lamu in l1magic 
 
-    let eta = compute_eta(&h_init,&u_vec); //sdg (surrogate duality gap)
-    let tau = compute_tau(mu,dual_dim as Float,eta);
-
-
-
+    let eta = eta(&h,&u_vec); //sdg (surrogate duality gap)
+    let tau = tau(mu,dual_dim as Float,eta);
 
 
+    let mut iter = 0;
+
+
+    while iter < max_iter && eta < tol {
+
+        let w2 = w2(&u_vec,&ones);
+        let (sigma_1,sigma_2,sigma_x) = sigmas(&u_vec, &h_recip);
+
+
+    }
 
 
     panic!("TODO");
@@ -49,7 +61,7 @@ fn dual_residual<const N: usize, const M: usize>(f_0_grad: &DVector<Float>,  A_t
     let lamu_2 = u_vec.fixed_rows::<M>(M);
 
     dh_u.fixed_rows_mut::<N>(0).copy_from(&(A_t*(lamu_1-lamu_2)));
-    dh_u.fixed_rows_mut::<M>(N).copy_from(&(lamu_1-lamu_2));
+    dh_u.fixed_rows_mut::<M>(N).copy_from(&(-lamu_1-lamu_2));
 
     f_0_grad+dh_u
 }
@@ -64,30 +76,53 @@ fn center_residual<>(h: &DVector<Float>, u_vec: &DVector<Float>, tau: Float) -> 
 }
 
 #[allow(non_snake_case)]
-fn compute_f_0<const N: usize, const M: usize>(measurements: &OVector<Float,Const<M>>, A: &OMatrix<Float, Const<M>,Const<N>>,  state: &OVector<Float,Const<N>>) -> OVector<Float,Const<M>> {
+fn f_0<const N: usize, const M: usize>(measurements: &OVector<Float,Const<M>>, A: &OMatrix<Float, Const<M>,Const<N>>,  state: &OVector<Float,Const<N>>) -> OVector<Float,Const<M>> {
     let res_abs = (A*state-measurements).abs();
     (0.95*res_abs).add_scalar(0.1*res_abs.max())
 }
 
 #[allow(non_snake_case)]
-fn compute_h<const N: usize, const M: usize>(measurements: &OVector<Float,Const<M>>, A: &OMatrix<Float, Const<M>,Const<N>>, state: &OVector<Float,Const<N>>, residual: &OVector<Float,Const<M>>) -> DVector<Float> {
+fn h<const N: usize, const M: usize>(measurements: &OVector<Float,Const<M>>, A: &OMatrix<Float, Const<M>,Const<N>>, state: &OVector<Float,Const<N>>, residual: &OVector<Float,Const<M>>) -> DVector<Float> {
     let f_1 = A*state-measurements - residual;
     let f_2 = -A*state+measurements -residual;
-    let mut h = DVector::<Float>::zeros(N*2);
+    let mut h = DVector::<Float>::zeros(M*2);
     h.fixed_rows_mut::<M>(0).copy_from(&f_1);
     h.fixed_rows_mut::<M>(M).copy_from(&f_2);
     h
 }
 
-fn compute_eta(h: &DVector<Float>, u_vec: &DVector<Float>) -> Float {
+fn eta(h: &DVector<Float>, u_vec: &DVector<Float>) -> Float {
     (-h.transpose()*u_vec)[(0,0)]
 }
 
-fn compute_tau(mu: Float, m: Float, eta: Float) -> Float {
+fn tau(mu: Float, m: Float, eta: Float) -> Float {
     mu*m/eta
 }
 
-fn compute_u(h: &DVector<Float>, tau: Float) -> DVector<Float> {
+fn u(h: &DVector<Float>, tau: Float) -> DVector<Float> {
     h.map(|v| -(v*tau).recip())
+}
+
+fn w2(u: &DVector<Float>,ones: &DVector<Float>) -> DVector<Float> {
+    ones-u
+}
+
+/**
+ * (sigma_1,sigma_2,sigma_x)
+ */
+fn sigmas(u: &DVector<Float>, h_recip: &DVector<Float>) -> (DVector<Float>,DVector<Float>,DVector<Float>) {
+    let subproblem_size = h_recip.nrows()/2;
+    let v = u*h_recip;
+    let v_1 = v.rows(0,subproblem_size);
+    let v_2 = v.rows(subproblem_size,subproblem_size);
+
+    let sigma_1 = -v_1 - v_2;
+    let sigma_1_recip = sigma_1.map(|x| x.recip());
+    let sigma_2 = v_1 - v_2;
+    let sigma_2_squared = sigma_2.map(|x| x.powi(2));
+
+    let sigma_x = sigma_1 - sigma_2_squared.component_mul(&sigma_1_recip);
+    (sigma_1,sigma_2,sigma_x)
+
 }
 
