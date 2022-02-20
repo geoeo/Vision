542f88efac6d72d257a1cd63a71fac61ba4d65e5
Marc Haubenstock <marc.haubenstock@outlook.com>
2022-02-20 21:10:19 +0100

    improved data loading

--- src/sfm/bundle_adjustment/mod.rs
+++ src/sfm/bundle_adjustment/mod.rs
@@ -17,6 +17,9 @@ pub mod state;
 pub fn run_ba<C : Camera + Copy, T : Feature>(all_matches: &Vec<Vec<Match<T>>>, camera_data: &Vec<((usize, C),(usize,C))>, 
                                 img_dim : (usize,usize) ,runtime_parameters: &RuntimeParameters, pyramid_scale: Float, use_esstial_decomp_for_initial_guess: bool ) 
                                 -> ((Vec<Isometry3<Float>>, Vec<Vector3<Float>>), (serde_yaml::Result<String>, serde_yaml::Result<String>)){
+
+
+
     let mut unique_cameras_sorted = Vec::<(usize, C)>::with_capacity(camera_data.len());
     for (a,b) in camera_data {
         unique_cameras_sorted.push(*a);
@@ -37,7 +40,7 @@ pub fn run_ba<C : Camera + Copy, T : Feature>(all_matches: &Vec<Vec<Match<T>>>, 
     let initial_cam_pos_guess: Option<Vec<(u64,(Vector3<Float>,Matrix3<Float>))>> = match use_esstial_decomp_for_initial_guess {
         false => None,
         true => {
-            let feature_machtes = all_matches.iter().map(|m| epipolar::extract_matches(m, pyramid_scale, false)).collect::<Vec<Vec<(Vector2<Float>,Vector2<Float>)>>>();
+            let feature_machtes = all_matches.iter().filter(|m| m.len() >= 8).map(|m| epipolar::extract_matches(m, pyramid_scale, false)).collect::<Vec<Vec<(Vector2<Float>,Vector2<Float>)>>>();
             let fundamental_matrices = feature_machtes.iter().map(|m| epipolar::eight_point(m)).collect::<Vec<epipolar::Fundamental>>();
             let normalized_matches = fundamental_matrices.iter().zip(feature_machtes.iter()).map(|(f,m)| epipolar::filter_matches(f, m)).collect::<Vec<Vec<(Vector3<Float>,Vector3<Float>)>>>();
             let essential_matrices = fundamental_matrices.iter().enumerate().map(|(i,f)| {
@@ -53,6 +56,8 @@ pub fn run_ba<C : Camera + Copy, T : Feature>(all_matches: &Vec<Vec<Match<T>>>, 
         
         } 
     };
+
+    //TODO filter features with epipolar check
 
     //TODO: switch impl
     let mut state = feature_map.get_euclidean_landmark_state(initial_cam_pos_guess, Vector3::<Float>::new(0.0,0.0,-1.0));
