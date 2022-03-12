dea5f3a08ce8d3c3cff0f7c4423c5b6267560dc5
Marc Haubenstock <marc.haubenstock@outlook.com>
2022-03-12 14:44:13 +0100

    fixed epipolar examlpe

--- examples/image_processing/epipolar.rs
+++ examples/image_processing/epipolar.rs
@@ -5,7 +5,7 @@ extern crate nalgebra as na;
 
 use std::fs;
 use color_eyre::eyre::Result;
-use vision::image::features::{Match,orb_feature::OrbFeature};
+use vision::image::features::{Match,orb_feature::OrbFeature,Feature};
 use vision::image::pyramid::orb::orb_runtime_parameters::OrbRuntimeParameters;
 use vision::image::epipolar;
 use vision::sensors::camera::{pinhole::Pinhole, Camera};
@@ -26,14 +26,16 @@ fn main() -> Result<()> {
 
     let feature_matches = epipolar::extract_matches(&matches[0], orb_params.pyramid_scale, false);
     let fundamental_matrix = epipolar::eight_point(&feature_matches);
-
+    let depth_prior = 1.0;
     let essential_matrix = epipolar::compute_essential(&fundamental_matrix, &intensity_camera_1.get_projection(), &intensity_camera_2.get_projection());
-    let normalized_matches = epipolar::filter_matches(&fundamental_matrix, &feature_matches, -1.0);
-    for (l,r) in &normalized_matches {
+    let normalized_matches = epipolar::filter_matches(&fundamental_matrix, &feature_matches, depth_prior,1.0);
+    for m in &normalized_matches {
+        let l = m.feature_one.get_as_3d_point(depth_prior);
+        let r = m.feature_two.get_as_3d_point(depth_prior);
         let t = l.transpose()*fundamental_matrix*r;
         println!("{}",t);
     }
-    let (h,R) = epipolar::decompose_essential_kanatani(&essential_matrix,&normalized_matches);
+    let (h,R) = epipolar::decompose_essential_kanatani(&essential_matrix,&normalized_matches,depth_prior);
     //let (h,R) = epipolar::decompose_essential_förstner(&essential_matrix,&normalized_matches);
 
     println!("{}",h);
