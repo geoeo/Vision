099ae69a5052a1a389dbcfb59378222abe22356f
Marc Haubenstock <marc.dhaubenstock@gmail.com>
2023-03-14 22:35:15 +0100

    starting to implement known rotation problem gen

--- src/sfm/outlier_rejection/mod.rs
+++ src/sfm/outlier_rejection/mod.rs
@@ -29,9 +29,9 @@ fn generate_known_rotation_problem<Feat: Feature + Clone>(unique_landmark_ids: &
     let mut b = DMatrix::<Float>::zeros(number_of_residuals,number_of_target_parameters);
     let mut c = DMatrix::<Float>::zeros(number_of_residuals,number_of_target_parameters);
 
-    let mut a0 = DVector::<Float>::zeros(number_of_residuals);
-    let mut b0 = DVector::<Float>::zeros(number_of_residuals);
-    let mut c0 = DVector::<Float>::zeros(number_of_residuals);
+    let a0 = DVector::<Float>::zeros(number_of_residuals);
+    let b0 = DVector::<Float>::zeros(number_of_residuals);
+    let c0 = DVector::<Float>::zeros(number_of_residuals);
 
     let mut row_acc = 0;
     // Skip root cam since we assume origin (Check this)
@@ -44,24 +44,44 @@ fn generate_known_rotation_problem<Feat: Feature + Clone>(unique_landmark_ids: &
         //let rows = 3*number_of_points*2;
         let ones = DVector::<Float>::repeat(number_of_points, 1.0);
 
-        let mut p_data = DVector::<Float>::zeros(number_of_points);
+        let mut p_data_x = DVector::<Float>::zeros(number_of_points);
+        let mut p_data_y = DVector::<Float>::zeros(number_of_points);
         let mut p_col_ids = DVector::<usize>::zeros(number_of_points);
         for (idx, feature) in features.iter().enumerate() {
-            p_data[idx] = feature.get_x_image_float();
+            p_data_x[idx] = feature.get_x_image_float();
+            p_data_y[idx] = feature.get_y_image_float();
             p_col_ids[idx] = feature.get_landmark_id().expect("generate_known_rotation_problem: no landmark id");
         }
-        let point_coeff = (&p_data)*rotation_matrix.row(2) - (&ones)*rotation_matrix.row(0);
+        let point_coeff_x = (&p_data_x)*rotation_matrix.row(2) - (&ones)*rotation_matrix.row(0);
+        let point_coeff_y = (&p_data_y)*rotation_matrix.row(2) - (&ones)*rotation_matrix.row(1); 
+        let point_coeff_z = (&ones)*rotation_matrix.row(2); 
 
-        let mut t_coeff = MatrixXx3::<Float>::zeros(number_of_points);
-        t_coeff.column_mut(0).copy_from(&(-(&ones)));
-        t_coeff.column_mut(2).copy_from(&p_data);
+        let mut t_coeff_x = MatrixXx3::<Float>::zeros(number_of_points);
+        let mut t_coeff_y = MatrixXx3::<Float>::zeros(number_of_points);
+        let mut t_coeff_z = MatrixXx3::<Float>::zeros(number_of_points);
+        t_coeff_x.column_mut(0).copy_from(&(-(&ones)));
+        t_coeff_y.column_mut(0).copy_from(&(-(&ones)));
+        t_coeff_x.column_mut(2).copy_from(&p_data_x);
+        t_coeff_y.column_mut(2).copy_from(&p_data_y);
+        t_coeff_z.column_mut(2).copy_from(&ones);
 
         for p_idx in 0..number_of_points{
-            let v_p = point_coeff.row(p_idx);
-            let v_t = t_coeff.row(p_idx);
+            let v_p_x = point_coeff_x.row(p_idx);
+            let v_p_y = point_coeff_y.row(p_idx);
+            let v_p_z = point_coeff_z.row(p_idx);
+            let v_t_x = t_coeff_x.row(p_idx);
+            let v_t_y = t_coeff_y.row(p_idx);
+            let v_t_z = t_coeff_z.row(p_idx);
             let col_id = p_col_ids[p_idx];
-            a.fixed_slice_mut::<1,3>(row_acc+p_idx,3*col_id).copy_from(&v_p);
-            a.fixed_slice_mut::<1,3>(row_acc+p_idx,3*(number_of_unique_points+(cam_idx-1))).copy_from(&v_t);
+            let r_idx = row_acc+p_idx;
+            let column_l_idx = 3*col_id;
+            let column_t_idx = 3*(number_of_unique_points+(cam_idx-1));
+            a.fixed_slice_mut::<1,3>(r_idx,column_l_idx).copy_from(&v_p_x);
+            a.fixed_slice_mut::<1,3>(r_idx,column_t_idx).copy_from(&v_t_x);
+            b.fixed_slice_mut::<1,3>(r_idx,column_l_idx).copy_from(&v_p_y);
+            b.fixed_slice_mut::<1,3>(r_idx,column_t_idx).copy_from(&v_t_y);
+            c.fixed_slice_mut::<1,3>(r_idx,column_l_idx).copy_from(&v_p_z);
+            c.fixed_slice_mut::<1,3>(r_idx,column_t_idx).copy_from(&v_t_z);
         }
 
 
