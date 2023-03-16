85c3afed8a6a2d2df2a05c17b1a5b0556d8db701
Marc Haubenstock <marc.dhaubenstock@gmail.com>
2023-03-16 22:27:59 +0100

    setup feasibility problem

--- src/sfm/outlier_rejection/mod.rs
+++ src/sfm/outlier_rejection/mod.rs
@@ -19,6 +19,23 @@ pub fn outlier_rejection_dual<Feat: Feature + Clone>(unique_landmark_ids: &HashS
     panic!("TODO");
 }
 
+fn sovlve_feasability_problem(a: &DMatrix<Float>, b: &DMatrix<Float>, c: &DMatrix<Float>, a0: &DVector<Float>, b0: &DVector<Float>, c0: &DVector<Float>, tol: Float, min_depth: Float, max_depth: Float) -> (DVector<Float>, DVector<Float>) {
+    let mut a1 = DMatrix::<Float>::zeros(2*a.nrows() + 2*b.nrows(),a.ncols());
+    a1.slice_mut((0, 0),a.shape()).copy_from(&(-a-tol*c));
+    a1.slice_mut((a.nrows(), 0),a.shape()).copy_from(&(a-tol*c));
+    a1.slice_mut((2*a.nrows(), 0),b.shape()).copy_from(&(-b-tol*c));
+    a1.slice_mut((2*a.nrows() + b.nrows(), 0),b.shape()).copy_from(&(b-tol*c));
+
+    let mut b1 = DVector::<Float>::zeros(2*a0.nrows() + 2*b0.nrows());
+    b1.rows_mut(0, a0.nrows()).copy_from(&(a0+tol*c0));
+    b1.rows_mut(a0.nrows(), a0.nrows()).copy_from(&(-a0+tol*c0));
+    b1.rows_mut(2*a0.nrows(), b0.nrows()).copy_from(&(b0+tol*c0));
+    b1.rows_mut(2*a0.nrows() + b0.nrows(), b0.nrows()).copy_from(&(-b0+tol*c0));
+
+
+    panic!("TODO")
+}
+
 fn generate_known_rotation_problem<Feat: Feature + Clone>(unique_landmark_ids: &HashSet<usize>, camera_ids_root_first: &Vec<usize>, abs_pose_map: &HashMap<usize,Isometry3<Float>>, feature_map: &HashMap<usize, HashSet<Feat>>) -> (DMatrix<Float>,DMatrix<Float>,DMatrix<Float>,DVector<Float>,DVector<Float>,DVector<Float>) {
     let number_of_unique_points = unique_landmark_ids.len();
     let number_of_poses = abs_pose_map.len();
@@ -34,14 +51,13 @@ fn generate_known_rotation_problem<Feat: Feature + Clone>(unique_landmark_ids: &
     let c0 = DVector::<Float>::zeros(number_of_residuals);
 
     let mut row_acc = 0;
-    // Skip root cam since we assume origin (Check this)
+    // Skip root cam since we assume origin (TOOD: Check this)
     for cam_idx in 1..number_of_poses {
         let cam_id = camera_ids_root_first[cam_idx];
         let rotation = abs_pose_map.get(&cam_id).expect("generate_known_rotation_problem: No rotation found").rotation.to_rotation_matrix();
         let rotation_matrix = rotation.matrix();
         let features = feature_map.get(&cam_id).expect("generate_known_rotation_problem: No features found");
         let number_of_points = features.len(); // asuming every feature's landmark id is distinct -> maybe make an explicit check?
-        //let rows = 3*number_of_points*2;
         let ones = DVector::<Float>::repeat(number_of_points, 1.0);
 
         let mut p_data_x = DVector::<Float>::zeros(number_of_points);
@@ -84,13 +100,8 @@ fn generate_known_rotation_problem<Feat: Feature + Clone>(unique_landmark_ids: &
             c.fixed_slice_mut::<1,3>(r_idx,column_t_idx).copy_from(&v_t_z);
         }
 
-
-
-
         row_acc += number_of_points; 
     }
 
-
-    //TODO
     (a,b,c,a0,b0,c0)
 }
