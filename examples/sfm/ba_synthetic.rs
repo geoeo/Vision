98c2ea34b6a143edcd749e7e5ca1e14f086d930e
Marc Haubenstock <marc.dhaubenstock@gmail.com>
2023-04-30 13:28:22 +0200

    config naming

--- examples/sfm/ba_synthetic.rs
+++ examples/sfm/ba_synthetic.rs
@@ -80,24 +80,25 @@ fn main() -> Result<()> {
     //let sfm_config = SFMConfig::new(2, &vec!(vec!(1,3)), camera_map, &HashMap::from([((2,1),matches_2_1),((1,3),matches_1_3)]),
     //let sfm_config = SFMConfig::new(2, &vec!(vec!(1)), camera_map.clone(), camera_map, vec!(vec!(matches_2_1)),
     //let sfm_config = SFMConfig::new(2, &vec!(vec!(3,4)), camera_map.clone(), camera_map, vec!(vec!(matches_2_3,matches_3_4)),
-    //let sfm_config = SFMConfig::new(2, &vec!(vec!(3)), camera_map.clone(), camera_map, vec!(vec!(matches_2_3)),
+    let sfm_config = SFMConfig::new(2, &vec!(vec!(3)), camera_map,  &HashMap::from([((2,3),matches_2_3)]),
     //let sfm_config = SFMConfig::new(2, &vec!(vec!(4)), camera_map.clone(), camera_map, vec!(vec!(matches_2_4)),
     //let sfm_config = SFMConfig::new(2, &vec!(vec!(1,0)), camera_map.clone(), camera_map, vec!(vec!(matches_2_1,matches_1_0)),
     //let sfm_config = SFMConfig::new(2, &vec!(vec!(1,0), vec!(3)), camera_map.clone(), camera_map, vec!(vec!(matches_2_1,matches_1_0),vec!(matches_2_3)),
     //let sfm_config = SFMConfig::new(2, &vec!(vec!(3,4), vec!(1)), camera_map.clone(), camera_map, vec!(vec!(matches_2_3,matches_3_4),vec!(matches_1_0)),
 
-    let sfm_config = SFMConfig::new(2, &vec!(vec!(1), vec!(3), vec!(4), vec!(0)), camera_map, &HashMap::from([((2,1),matches_2_1),((2,3),matches_2_3),((2,4),matches_2_4),((2,0),matches_2_0)]),
+    //let sfm_config = SFMConfig::new(2, &vec!(vec!(1), vec!(3), vec!(4), vec!(0)), camera_map, &HashMap::from([((2,1),matches_2_1),((2,3),matches_2_3),((2,4),matches_2_4),((2,0),matches_2_0)]),
+    //let sfm_config = SFMConfig::new(2, &vec!(vec!(3), vec!(4), vec!(0)), camera_map, &HashMap::from([((2,1),matches_2_1),((2,3),matches_2_3),((2,4),matches_2_4),((2,0),matches_2_0)]),
     //let sfm_config = SFMConfig::new(2, &vec!(vec!(1,4), vec!(0)), camera_map, &HashMap::from([((2,1),matches_2_1),((1,4),matches_1_4),((2,0),matches_2_0)]),
     //let sfm_config = SFMConfig::new(2, &vec!(vec!(1,0,3,4)), camera_map, &HashMap::from([((2,1),matches_2_1),((1,0),matches_1_0),((0,3),matches_0_3),((3,4),matches_3_4)]),
     //let sfm_config = SFMConfig::new(3, &vec!(vec!(4)), camera_map.clone(), camera_map, vec!(vec!(matches_3_4)),
 
-    BifocalType::ESSENTIAL,  Triangulation::STEREO, perc_tresh, 1e0, float::INFINITY, 5.0, refine_rotation_via_rcd, positive_principal_distance);
+    BifocalType::ESSENTIAL,  Triangulation::LINEAR, perc_tresh, 1e-1, float::INFINITY, 5.0, refine_rotation_via_rcd, positive_principal_distance);
 
     let ((cam_positions,points),(s,debug_states_serialized)) = run_ba(&sfm_config, (480,640), &runtime_parameters);
     //let ((cam_positions,points),(s,debug_states_serialized)) = run_ba(&sfm_config.matches(), &sfm_config, None, (480,640), &runtime_parameters, 1.0,depth_prior);
-    fs::write(format!("{}/{}",runtime_conf.output_path,"3dv.txt"), s?).expect("Unable to write file");
+    fs::write(format!("{}/{}",runtime_conf.output_path,"ba_synthetic.txt"), s?).expect("Unable to write file");
     if runtime_parameters.debug {
-        fs::write(format!("{}/{}",runtime_conf.output_path,"3dv_debug.txt"), debug_states_serialized?).expect("Unable to write file");
+        fs::write(format!("{}/{}",runtime_conf.output_path,"ba_synthetic_debug.txt"), debug_states_serialized?).expect("Unable to write file");
     }
    
     Ok(())
--- examples/visualizing/pointcloud.rs
+++ examples/visualizing/pointcloud.rs
@@ -57,11 +57,11 @@ fn main() -> Result<(),()> {
     let mut window = Window::new("BA: Pointcloud");
     let runtime_conf = load_runtime_conf();
 
-    // let final_state_as_string = fs::read_to_string(format!("{}/3dv.txt",runtime_conf.output_path)).expect("Unable to read file");
-    // let all_states_as_string_option = fs::read_to_string(format!("{}/3dv_debug.txt",runtime_conf.output_path));
+    let final_state_as_string = fs::read_to_string(format!("{}/ba_synthetic.txt",runtime_conf.output_path)).expect("Unable to read file");
+    let all_states_as_string_option = fs::read_to_string(format!("{}/ba_synthetic_debug.txt",runtime_conf.output_path));
 
-    let final_state_as_string = fs::read_to_string(format!("{}/olsen.txt", runtime_conf.output_path)).expect("Unable to read file");
-    let all_states_as_string_option = fs::read_to_string(format!("{}/olsen_debug.txt", runtime_conf.output_path));
+/*     let final_state_as_string = fs::read_to_string(format!("{}/olsen.txt", runtime_conf.output_path)).expect("Unable to read file");
+    let all_states_as_string_option = fs::read_to_string(format!("{}/olsen_debug.txt", runtime_conf.output_path)); */
 
     let loaded_state: (Vec<[Float;6]>,Vec<[Float;3]>) = serde_yaml::from_str(&final_state_as_string).unwrap();
     let ba_state = state::State::<Float,EuclideanLandmark<Float>,3>::from_serial(&loaded_state);
